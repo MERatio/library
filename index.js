50c116ba814e2d552fbe560d1d30715ad437b489
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2022-10-23 18:09:31 +0800

    Add JS functionality that can add dummy books into array

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 'use strict';
 
-alert('test');
+let library = [];
+
+function Book(title, author, numOfPages, read) {
+	this.title = title;
+	this.author = author;
+	this.numOfPages = numOfPages;
+	this.read = read;
+}
+
+function addBookToLibrary(title, author, numOfPages, read) {
+	const newBook = new Book(title, author, numOfPages, read);
+	library = [...library, newBook];
+}
+
+function addDummyBooks(count) {
+	for (let i = 0; i < count; i++) {
+		addBookToLibrary(`title ${i}`, `author ${i}`, 300 + i, i % 2 === 0);
+	}
+}
+
+if (library.length === 0) {
+	addDummyBooks(3);
+}

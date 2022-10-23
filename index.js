f91f2534db7b7422b02617f9c10d38e84f6a2e7a
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2022-10-23 21:59:04 +0800

    Render books

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const dom = {
+	cards: document.querySelector('.js-cards'),
+};
+
 let library = [];
 
 function Book(title, author, numOfPages, read) {
@@ -14,6 +18,40 @@ function addBookToLibrary(title, author, numOfPages, read) {
 	library = [...library, newBook];
 }
 
+function createDomBook(book) {
+	const domBook = document.createElement('article');
+	domBook.classList.add('card');
+
+	const domTitle = document.createElement('h2');
+	domTitle.classList.add('card-title');
+	domTitle.textContent = book.title;
+	domBook.appendChild(domTitle);
+
+	const domAuthor = document.createElement('p');
+	domAuthor.classList.add('card-text');
+	domAuthor.textContent = `by ${book.author}`;
+	domBook.appendChild(domAuthor);
+
+	const domNumOfPages = document.createElement('p');
+	domNumOfPages.classList.add('card-text');
+	domNumOfPages.textContent = `${book.numOfPages} pages`;
+	domBook.appendChild(domNumOfPages);
+
+	const domRead = document.createElement('p');
+	domRead.classList.add('card-text');
+	domRead.textContent = book.read ? 'read' : 'not read yet';
+	domBook.appendChild(domRead);
+
+	return domBook;
+}
+
+function renderLibrary(library) {
+	for (const book of library) {
+		const domBook = createDomBook(book);
+		dom.cards.appendChild(domBook);
+	}
+}
+
 function addDummyBooks(count) {
 	for (let i = 0; i < count; i++) {
 		addBookToLibrary(`title ${i}`, `author ${i}`, 300 + i, i % 2 === 0);
@@ -21,5 +59,6 @@ function addDummyBooks(count) {
 }
 
 if (library.length === 0) {
-	addDummyBooks(3);
+	addDummyBooks(5);
+	renderLibrary(library);
 }

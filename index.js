5dbe70039c01610a2d32d34c72d7137280265591
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2023-10-15 11:13:10 +0800

    Add Book(), addBook(), and seedBook()

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 'use strict';
 
-console.log('test');
+const books = [];
+
+function Book(author, title, pages, read) {
+	this.author = author;
+	this.title = title;
+	this.pages = pages;
+	this.read = read;
+}
+
+function addBook(author, title, pages, read) {
+	const book = new Book(author, title, pages, read);
+	books.push(book);
+}
+
+function seedBooks(numOfBooks) {
+	for (let i = 1; i <= numOfBooks; i++) {
+		addBook(`author-${i}`, `book-${i}`, i * 100, Math.random() < 0.5);
+	}
+}
+
+seedBooks(10);

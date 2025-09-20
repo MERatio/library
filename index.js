48a0d72acec9cc64087b25ae1cf8e51758a743a8
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2025-09-20 12:10:44 +0800

    Place book related stuff in Book class

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ const dom = {
   addBookForm: document.querySelector('#addBookForm'),
   booksList: document.querySelector('#booksList'),
 };
-const books = [];
 
 class Book {
+  static #books = [];
+
   constructor(title, author, pages, haveRead) {
     this.id = crypto.randomUUID();
     this.title = title;
@@ -21,74 +22,70 @@ class Book {
   changeHaveRead() {
     this.haveRead = !this.haveRead;
   }
-}
 
-function addBookToBooks(title, author, pages, haveRead) {
-  const book = new Book(title, author, pages, haveRead);
-  books.push(book);
-}
-
-function createDomBook(book) {
-  const domBook = document.createElement('li');
-  domBook.classList.add('books-list-item');
-
-  const domBookTitle = document.createElement('p');
-  domBookTitle.classList.add('book-title');
-  domBookTitle.textContent = book.title;
-  domBook.appendChild(domBookTitle);
-
-  const domBookAuthor = document.createElement('p');
-  domBookAuthor.textContent = book.author;
-  domBook.appendChild(domBookAuthor);
-
-  const domBookPages = document.createElement('p');
-  domBookPages.textContent = `${book.pages} pages`;
-  domBook.appendChild(domBookPages);
-
-  const domReadBookBtn = document.createElement('button');
-  domReadBookBtn.classList.add('read-book-btn', 'readBookBtn');
-  domReadBookBtn.setAttribute('type', 'button');
-  domReadBookBtn.dataset.bookId = book.id;
-  if (book.haveRead) {
-    domBook.dataset.read = 'true';
-    domReadBookBtn.textContent = 'Read';
-  } else {
-    domBook.dataset.read = 'false';
-    domReadBookBtn.textContent = 'Not read';
+  static addBookToBooks(title, author, pages, haveRead) {
+    const book = new Book(title, author, pages, haveRead);
+    this.#books.push(book);
   }
-  domReadBookBtn.addEventListener('click', (e) => {
-    const bookId = e.currentTarget.dataset.bookId;
-    const book = books.find((book) => book.id === bookId);
-    book.changeHaveRead();
-    displayDomBooks(books);
-  });
-  domBook.appendChild(domReadBookBtn);
 
-  const domRemoveBookBtn = document.createElement('button');
-  domRemoveBookBtn.classList.add('remove-book-btn', 'removeBookBtn');
-  domRemoveBookBtn.setAttribute('type', 'button');
-  domRemoveBookBtn.dataset.bookId = book.id;
-  domRemoveBookBtn.textContent = 'Remove';
-  domRemoveBookBtn.addEventListener('click', (e) => {
-    const bookId = e.currentTarget.dataset.bookId;
-    const bookIndex = books.findIndex((book) => book.id === bookId);
-    books.splice(bookIndex, 1);
-    displayDomBooks(books);
-  });
-  domBook.appendChild(domRemoveBookBtn);
+  static createDomBook(book) {
+    const domBook = document.createElement('li');
+    domBook.classList.add('books-list-item');
 
-  return domBook;
-}
+    const domBookTitle = document.createElement('p');
+    domBookTitle.classList.add('book-title');
+    domBookTitle.textContent = book.title;
+    domBook.appendChild(domBookTitle);
 
-function clearDomBookList() {
-  dom.booksList.replaceChildren();
-}
+    const domBookAuthor = document.createElement('p');
+    domBookAuthor.textContent = book.author;
+    domBook.appendChild(domBookAuthor);
 
-function displayDomBooks(books) {
-  clearDomBookList();
-  for (const book of books) {
-    const domBook = createDomBook(book);
-    dom.booksList.appendChild(domBook);
+    const domBookPages = document.createElement('p');
+    domBookPages.textContent = `${book.pages} pages`;
+    domBook.appendChild(domBookPages);
+
+    const domReadBookBtn = document.createElement('button');
+    domReadBookBtn.classList.add('read-book-btn', 'readBookBtn');
+    domReadBookBtn.setAttribute('type', 'button');
+    domReadBookBtn.dataset.bookId = book.id;
+    if (book.haveRead) {
+      domBook.dataset.read = 'true';
+      domReadBookBtn.textContent = 'Read';
+    } else {
+      domBook.dataset.read = 'false';
+      domReadBookBtn.textContent = 'Not read';
+    }
+    domReadBookBtn.addEventListener('click', (e) => {
+      const bookId = e.currentTarget.dataset.bookId;
+      const book = this.#books.find((book) => book.id === bookId);
+      book.changeHaveRead();
+      this.displayDomBooks();
+    });
+    domBook.appendChild(domReadBookBtn);
+
+    const domRemoveBookBtn = document.createElement('button');
+    domRemoveBookBtn.classList.add('remove-book-btn', 'removeBookBtn');
+    domRemoveBookBtn.setAttribute('type', 'button');
+    domRemoveBookBtn.dataset.bookId = book.id;
+    domRemoveBookBtn.textContent = 'Remove';
+    domRemoveBookBtn.addEventListener('click', (e) => {
+      const bookId = e.currentTarget.dataset.bookId;
+      const bookIndex = this.#books.findIndex((book) => book.id === bookId);
+      this.#books.splice(bookIndex, 1);
+      this.displayDomBooks();
+    });
+    domBook.appendChild(domRemoveBookBtn);
+
+    return domBook;
+  }
+
+  static displayDomBooks() {
+    dom.booksList.replaceChildren();
+    for (const book of this.#books) {
+      const domBook = Book.createDomBook(book);
+      dom.booksList.appendChild(domBook);
+    }
   }
 }
 
@@ -105,14 +102,13 @@ dom.addBookForm.addEventListener('submit', (e) => {
   const author = dom.addBookForm.querySelector('#author').value;
   const pages = parseInt(dom.addBookForm.querySelector('#pages').value);
   const haveRead = dom.addBookForm.querySelector('#haveRead').checked;
-  const book = new Book(title, author, pages, haveRead);
-  books.push(book);
-  displayDomBooks(books);
+  Book.addBookToBooks(title, author, pages, haveRead);
+  Book.displayDomBooks();
   dom.addBookForm.reset();
 });
 
-addBookToBooks('Atomic Habits', 'James Clear', 320, true);
-addBookToBooks('A Game of Thrones', 'George R. R. Martin', 694, false);
-addBookToBooks('The Lord of the Rings', 'J. R. R. Tolkien', 1077, false);
+Book.addBookToBooks('Atomic Habits', 'James Clear', 320, true);
+Book.addBookToBooks('A Game of Thrones', 'George R. R. Martin', 694, false);
+Book.addBookToBooks('The Lord of the Rings', 'J. R. R. Tolkien', 1077, false);
 
-displayDomBooks(books);
+Book.displayDomBooks();
